13f038b359ef6866cd1490f00516c84359421425
aayanrahman <76698553+aayanrahman@users.noreply.github.com>
2024-08-15 16:52:17 -0400

    working database

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"; // Change this line
+
+const appSettings = {
+    databaseURL: "https://shopping-list-39a23-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", () => {
-    const input = inputFieldEl.value
-    console.log(input)
-})
+    const input = inputFieldEl.value;
+
+    push(shoppingListInDB, input);
+    console.log(input);
+});
 
